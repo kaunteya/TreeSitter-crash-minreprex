be458fef5b124eecc9c3ccc5a3af3e9889a983eb
Kaunteya <k.suryawanshi@gmail.com>
2023-08-06 22:32:46 +0530

    Initial Commit

---
+++ TreeSitter-Crash/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  TreeSitter-Crash
+//
+//  Created by Kauntey Suryawanshi on 06/08/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ TreeSitter-Crash/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TreeSitter-Crash
+//
+//  Created by Kauntey Suryawanshi on 06/08/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
